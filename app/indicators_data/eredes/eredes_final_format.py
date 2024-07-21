24b3dfbde6bbfbd5f78d1badb54b6a15677e8f88
Roger Noguera Planesas <rogernp1036@gmail.com>
2024-07-22 00:45:32 +0200

    Added geolocation to the eredes data

--- app/indicators_data/eredes/eredes_final_format.py
+++ app/indicators_data/eredes/eredes_final_format.py
@@ -20,15 +20,19 @@ quarter_cols = ["Quarter"]
 #....................TIMECODE.....................
 
 def clean_date(value):
-    return re.sub(r'\W+', '', value)
+    # Eliminar cualquier símbolo y cualquier letra que no sea S o Q
+    value = re.sub(r'[^SQsq0-9]', '', value)
+    return value.upper()
 
 def combine_year_with_period(year, period):
     return year + period
 
-
-
-#### PROBLEMA!!!! SEMBLA QUE LLEGEIX ELS HEADERS I AGAFA POSICIONS INCORRECTES JA QUE ESCRIU DADES D'ALTRES COLUMNES
-
+def extract_date(value):
+    # Extraer solo la parte de año, mes y día, ignorando la hora
+    date_match = re.match(r"(\d{4}-\d{2}-\d{2})", value)
+    if date_match:
+        return date_match.group(1).replace("-", "")
+    return clean_date(value)
 
 def add_timecode(temp_merged, output_path):
     for filename in os.listdir(temp_merged):
@@ -64,31 +68,28 @@ def add_timecode(temp_merged, output_path):
                 # Buscar la columna 'Date'
                 for idx, col in enumerate(headers):
                     if any(dc.lower() in col.lower() for dc in date_cols):
-                        date_idx = idx
+                        date_idx = idx - 1
                         break
 
                 # Si hay columna 'Date', procesar solo esa columna
                 if date_idx is not None:
                     for row in rows:
                         new_row = row.copy()
-                        timecode = clean_date(row[date_idx])
+                        timecode = extract_date(row[date_idx])
                         new_row.insert(0, timecode)
                         new_rows.append(new_row)
-
                 else:
                     # Buscar columnas 'Year', 'Month', 'Semester', 'Quarter'
                     for idx, col in enumerate(headers):
                         col_lower = col.lower()
-                        if date_idx is None and any(dc.lower() in col_lower for dc in date_cols):
-                            date_idx = idx
-                        elif year_idx is None and any(yc.lower() in col_lower for yc in year_cols):
-                            year_idx = idx
+                        if year_idx is None and any(yc.lower() in col_lower for yc in year_cols):
+                            year_idx = idx - 1
                         elif month_idx is None and any(mc.lower() in col_lower for mc in month_cols):
-                            month_idx = idx
+                            month_idx = idx - 1
                         elif semester_idx is None and any(sc.lower() in col_lower for sc in semester_cols):
-                            semester_idx = idx
+                            semester_idx = idx - 1
                         elif quarter_idx is None and any(qc.lower() in col_lower for qc in quarter_cols):
-                            quarter_idx = idx
+                            quarter_idx = idx - 1
 
                     # Procesar combinación de 'Year' con 'Month', 'Semester' o 'Quarter'
                     for row in rows:
@@ -97,17 +98,25 @@ def add_timecode(temp_merged, output_path):
 
                         if year_idx is not None:
                             year_value = row[year_idx] if year_idx < len(row) else ""
-                            if month_idx is not None and month_idx < len(row) and len(row[month_idx]) <= 2:
-                                month_value = row[month_idx]
+                            if month_idx is not None and month_idx < len(row):
+                                month_value = row[month_idx].zfill(2)
                                 timecode = combine_year_with_period(year_value, month_value)
-                            elif semester_idx is not None and semester_idx < len(row) and len(row[semester_idx]) <= 2:
-                                semester_value = row[semester_idx]
+                            elif semester_idx is not None and semester_idx < len(row):
+                                semester_value = row[semester_idx].zfill(2)
+                                if len(semester_value) == 1:
+                                    semester_value = 'S' + semester_value
                                 timecode = combine_year_with_period(year_value, semester_value)
-                            elif quarter_idx is not None and quarter_idx < len(row) and len(row[quarter_idx]) <= 2:
-                                quarter_value = row[quarter_idx]
+                            elif quarter_idx is not None and quarter_idx < len(row):
+                                quarter_value = row[quarter_idx].zfill(2)
+                                if len(quarter_value) == 1:
+                                    quarter_value = 'Q' + quarter_value
                                 timecode = combine_year_with_period(year_value, quarter_value)
-                            elif quarter_idx is not None:
-                                timecode = clean_date(row[quarter_idx])
+                            else:
+                                timecode = year_value
+                        elif quarter_idx is not None and quarter_idx < len(row):
+                            timecode = clean_date(row[quarter_idx])
+                        elif semester_idx is not None and semester_idx < len(row):
+                            timecode = clean_date(row[semester_idx])
 
                         new_row.insert(0, timecode)
                         new_rows.append(new_row)
@@ -138,10 +147,9 @@ def format_zipcode_dict():
     return zip_dict
 
 def load_nuts_data():
-    with open(nuts_path, 'r', encoding = 'utf-8') as nuts_file:
+    with open(nuts_path, 'r', encoding='utf-8') as nuts_file:
         nuts_dict = json.load(nuts_file)
     return nuts_dict
-
 
 def get_location_data(zipcode, zip_dict):
     zipcode_str = str(zipcode)
@@ -151,99 +159,97 @@ def get_location_data(zipcode, zip_dict):
     freguesia = 'undefined'
 
     if len(zipcode_str) == 6:
-        # Buscar en el diccionario de zipcodes para zipcode completo de 6 cifras
         location_data = zip_dict.get(zipcode_str, {})
-        # Extraer 'distrito', 'concelho' y 'freguesia' si están disponibles
         distrito = location_data.get('distrito', 'undefined')
         concelho = location_data.get('concelho', 'undefined')
         freguesia = location_data.get('freguesia', 'undefined')
-
     elif len(zipcode_str) == 4:
-            for key, data in zip_dict.items():
-                if key.startswith(zipcode_str):
-                    location_data = data
-                    break
-            # Extraer 'distrito' y 'concelho' si están disponibles
-            distrito = location_data.get('distrito', 'undefined')
-            concelho = location_data.get('concelho', 'undefined')
-
+        for key, data in zip_dict.items():
+            if key.startswith(zipcode_str):
+                location_data = data
+                break
+        distrito = location_data.get('distrito', 'undefined')
+        concelho = location_data.get('concelho', 'undefined')
     elif len(zipcode_str) == 2:
-            for key, data in zip_dict.items():
-                if key.startswith(zipcode_str):
-                    location_data = data
-                    break
-            # Extraer solo 'distrito' si está disponible
-            distrito = location_data.get('distrito', 'undefined')
-    else:
-        # Código postal con longitud incorrecta
-        return distrito, concelho, freguesia
-    
-    print(distrito, concelho, freguesia)
+        for key, data in zip_dict.items():
+            if key.startswith(zipcode_str):
+                location_data = data
+                break
+        distrito = location_data.get('distrito', 'undefined')
     return distrito, concelho, freguesia
-
 
 def get_nuts_data(area, nuts_dict):
     nuts1 = 'PT'
     nuts2 = 'undefined'
     nuts3 = 'undefined'
 
-    # Iterar a través de todas las regiones principales
     for region, nuts_info in nuts_dict.items():
-        # Iterar a través de las regiones NUTS 2
         for region_nuts2, nuts2_info in nuts_info.get("NUTS 2", {}).items():
-            # Verificar si el área está en los municipios de NUTS 2
             for nuts3_region, nuts3_data in nuts2_info.get("NUTS 3", {}).items():
                 if area in nuts3_data.get("Municipalities", []):
                     nuts2 = region_nuts2
                     nuts3 = nuts3_region
-                    print(nuts1, nuts2, nuts3)
                     return nuts1, nuts2, nuts3
-
-            # Si no se encuentra en los municipios, verificar las regiones NUTS 3
             for nuts3_region in nuts2_info.get("NUTS 3", {}).keys():
                 if area == nuts3_region:
                     nuts2 = region_nuts2
                     nuts3 = nuts3_region
-                    print(nuts1, nuts2, nuts3)
                     return nuts1, nuts2, nuts3
-
-            # Si no se encuentra en las regiones NUTS 3, verificar NUTS 2
             if area == region_nuts2:
                 nuts2 = region_nuts2
-                print(nuts1, nuts2, nuts3)
                 return nuts1, nuts2, nuts3
     return nuts1, nuts2, nuts3
 
+def add_geodata():
+    zipcode_dict = format_zipcode_dict()
+    nuts_dict = load_nuts_data()
+
+    for filename in os.listdir(output_path):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(output_path, filename)
+            print(f"Procesando archivo: {file_path}")
+
+            with open(file_path, 'r', encoding='utf-8') as csv_file:
+                content = csv_file.read().lstrip('\ufeff')
+                csv_file.seek(0)
+                headers = content.splitlines()[0].split(';')
+                rows = [line.split(';') for line in content.splitlines()[1:]]
+
+                headers += ['distrito', 'concelho', 'freguesia', 'nuts1', 'nuts2', 'nuts3']
+
+                new_rows = []
+
+                for row in rows:
+                    zipcode = None
+                    for col_name in ['Zip Code', 'ZipCode', 'DistrictMunicipalityParishCode', 'CodDistritoConcelhoFreguesia', 'DistrictMunicipalityCode', 'CodConcelho', 'CODIGO_CONCELHO']:
+                        if col_name in headers:
+                            idx = headers.index(col_name)
+                            if row[idx] not in ['OUTROS', '', None]:
+                                value = row[idx]
+                                value = re.sub(r'[^0-9]', '', value)
+                                if len(value) in [2, 4, 6]:
+                                    zipcode = value
+                                    break
+
+                    if zipcode:
+                        distrito, concelho, freguesia = get_location_data(zipcode, zip_dict=zipcode_dict)
+                        area = concelho if concelho != 'undefined' else distrito
+                        nuts1, nuts2, nuts3 = get_nuts_data(area, nuts_dict=nuts_dict)
+                        if nuts2 != 'undefined' and nuts3 != 'undefined' and area == distrito:
+                            nuts3 = 'undefined'
+                    else:
+                        distrito, concelho, freguesia = 'undefined', 'undefined', 'undefined'
+                        nuts1, nuts2, nuts3 = 'PT', 'undefined', 'undefined'
+
+                    row += [distrito, concelho, freguesia, nuts1, nuts2, nuts3]
+                    new_rows.append(row)
+
+            with open(file_path, 'w', encoding='utf-8', newline='') as csv_file:
+                writer = csv.writer(csv_file, delimiter=';')
+                writer.writerow(headers)
+                writer.writerows(new_rows)
 
 
-
+# Ejecutar la función
 add_timecode(temp_merged=temp_merged, output_path=output_path)
-
-
-
-
-    # Cargar los diccionarios
-    #zipcode_dict = format_zipcode_dict()
-    #nuts_dict = load_nuts_data()
-
-    # Obtener los datos de ubicación (distrito y concelho)
-    #distrito, concelho, freguesia = get_location_data("010109", zip_dict=zipcode_dict)
-
-    # Determinar la ubicación a usar para get_nuts_data
-    #area = concelho if concelho != 'undefined' else distrito
-
-    # Obtener los datos de NUTS basados en la ubicación determinada
-    #nuts1, nuts2, nuts3 = get_nuts_data(area=area, nuts_dict=nuts_dict)
-
-    # Verificar si el área es igual al distrito para ajustar nuts3
-    #if nuts2 != 'undefined' and nuts3 != 'undefined' and area == distrito:
-        #nuts3 = 'undefined'
-
-    # Mostrar los resultados
-    #print(f"Distrito: {distrito}, Concelho: {concelho}, Freguesia: {freguesia}")
-    #print(f"NUTS1: {nuts1}, NUTS2: {nuts2}, NUTS3: {nuts3}")
-
-
-
-
-
+add_geodata()
