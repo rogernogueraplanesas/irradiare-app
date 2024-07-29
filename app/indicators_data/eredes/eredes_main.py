b17df66337322ff5bb17ddf6629cb4ea7b91a964
Roger Noguera Planesas <rogernp1036@gmail.com>
2024-07-29 11:31:09 +0200

    Solved Permission Error in eredes_final_format

--- app/indicators_data/eredes/eredes_main.py
+++ app/indicators_data/eredes/eredes_main.py
@@ -2,15 +2,15 @@ from data_extraction import eredes_data, eredes_metadata
 from data_processing import eredes_merge_files, eredes_final_format
 
 def eredes_main():
-    try:
-        eredes_data.main()
-    except Exception as e:
-        print(f"Error en eredes_data.main(): {e}")
+    #try:
+        #eredes_data.main()
+    #except Exception as e:
+        #print(f"Error en eredes_data.main(): {e}")
     
-    try:
-        eredes_metadata.main()
-    except Exception as e:
-        print(f"Error en eredes_metadata.main(): {e}")
+    #try:
+        #eredes_metadata.main()
+    #except Exception as e:
+        #print(f"Error en eredes_metadata.main(): {e}")
 
     try:
         eredes_merge_files.main()
